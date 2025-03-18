051a7b3aa5bf1202fb1dbf4a695706fcaa43fb71
Brian Williams <email.brianwilliams@gmail.com>
2025-03-18 16:48:35 +0000

    bah missing commit

--- Hawaii.Test/Nodes/FeatureNode.cs
+++ Hawaii.Test/Nodes/FeatureNode.cs
@@ -40,8 +40,8 @@ public class FeatureNode : MarkerNode
         {
             Position = new Vector2(0f, -HANDLE_OFFSET),
         };
-        //TranslationHandle.Clicked += OnTranslationHandleClicked;
-        //TranslationHandle.Dragged += OnTranslationHandleDragged;
+        TranslationHandle.Clicked += OnTranslationHandleClicked;
+        TranslationHandle.Dragged += OnTranslationHandleDragged;
         AddChild(TranslationHandle);
 
         RotationHandle = new FeatureHandleNode(Scene);
@@ -52,87 +52,86 @@ public class FeatureNode : MarkerNode
         {
             Position = new Vector2(0f, HANDLE_OFFSET),
         };
-        //RotationHandle.Clicked += OnRotationHandleClicked;
-        //RotationHandle.Dragged += OnRotationHandleDragged;
+        RotationHandle.Clicked += OnRotationHandleClicked;
+        RotationHandle.Dragged += OnRotationHandleDragged;
 
         AddChild(RotationHandle);
     }
 
-    //public override bool OnClicked(TouchEventData touchData)
-    //{
-    //    // Console.WriteLine($"Clicked at Local: {touchData.LocalPoint}, Parent: {touchData.ParentPoint}, World: {touchData.WorldPoint}");
-    //    WasClicked = true;
-    //    return true;
-    //}
+    public override bool OnClicked(TouchEventData touchData)
+    {
+        WasClicked = true;
+        return true;
+    }
 
-    //public override bool OnTouchUp(TouchEventData touchData)
-    //{
-    //    WasClicked = false;
-    //    return true;
-    //}
+    public override bool OnTouchUp(TouchEventData touchData)
+    {
+        WasClicked = false;
+        return true;
+    }
 
-    //public override bool OnDrag(TouchEventData touchData, PointF localDelta)
-    //{
-    //    var worldTransform = Scene.GetWorldTransform(Id);
-    //    var handleWorldPos = Vector2.Transform(Vector2.Zero, worldTransform);
-    //    var cursorWorldPos = new Vector2(touchData.WorldPoint.X, touchData.WorldPoint.Y);
-    //    var delta = cursorWorldPos - handleWorldPos; // Move center so handle hits cursor
-    //    Translate(delta, Space.World);
+    public override bool OnDrag(TouchEventData touchData, PointF localDelta)
+    {
+        var worldTransform = Scene.GetWorldTransform(Id);
+        var handleWorldPos = Vector2.Transform(Vector2.Zero, worldTransform);
+        var cursorWorldPos = new Vector2(touchData.WorldPoint.X, touchData.WorldPoint.Y);
+        var delta = cursorWorldPos - handleWorldPos; // Move center so handle hits cursor
+        Translate(delta, Space.World);
 
-    //    return true;
-    //}
+        return true;
+    }
 
-    //private void OnTranslationHandleClicked(TouchEventData touchData)
-    //{
-    //    TranslationHandle.WasClicked = !TranslationHandle.WasClicked;
-    //}
+    private void OnTranslationHandleClicked(TouchEventData touchData)
+    {
+        TranslationHandle.WasClicked = !TranslationHandle.WasClicked;
+    }
 
-    //private void OnTranslationHandleDragged((TouchEventData touchData, PointF localDelta) e)
-    //{
-    //    var worldTransform = Scene.GetWorldTransform(Id);
-    //    var handleLocalPos = new Vector2(0, -HANDLE_OFFSET); // Handle offset from center
-    //    var handleWorldPos = Vector2.Transform(handleLocalPos, worldTransform);
-    //    var cursorWorldPos = new Vector2(e.touchData.WorldPoint.X, e.touchData.WorldPoint.Y);
-    //    var delta = cursorWorldPos - handleWorldPos; // Move center so handle hits cursor
-    //    Translate(delta, Space.Local);
-    //}
+    private void OnTranslationHandleDragged((TouchEventData touchData, PointF localDelta) e)
+    {
+        var worldTransform = Scene.GetWorldTransform(Id);
+        var handleLocalPos = new Vector2(0, -HANDLE_OFFSET); // Handle offset from center
+        var handleWorldPos = Vector2.Transform(handleLocalPos, worldTransform);
+        var cursorWorldPos = new Vector2(e.touchData.WorldPoint.X, e.touchData.WorldPoint.Y);
+        var delta = cursorWorldPos - handleWorldPos; // Move center so handle hits cursor
+        Translate(delta, Space.Local);
+    }
 
-    //private void OnRotationHandleClicked(TouchEventData touchData)
-    //{
-    //    Console.WriteLine($"Rot Clicked at Local: {touchData.LocalPoint}, Parent: {touchData.ParentPoint}, World: {touchData.WorldPoint}");
-    //    RotationHandle.WasClicked = !RotationHandle.WasClicked;
-    //}
+    private void OnRotationHandleClicked(TouchEventData touchData)
+    {
+        Console.WriteLine($"Rot Clicked at Local: {touchData.LocalPoint}, Parent: {touchData.ParentPoint}, World: {touchData.WorldPoint}");
+        RotationHandle.WasClicked = !RotationHandle.WasClicked;
+    }
 
 
-    //private void OnRotationHandleDragged((TouchEventData touchData, PointF localDelta) e)
-    //{
-    //    Console.WriteLine($"Rot Dragged at Local: {e.touchData.LocalPoint}, Parent: {e.touchData.ParentPoint}, World: {e.touchData.WorldPoint}");
+    private void OnRotationHandleDragged((TouchEventData touchData, PointF localDelta) e)
+    {
+        Console.WriteLine($"Rot Dragged at Local: {e.touchData.LocalPoint}, Parent: {e.touchData.ParentPoint}, World: {e.touchData.WorldPoint}");
 
-    //    var worldTransform = Scene.GetParentTransform(Id);
+        var worldTransform = Scene.GetParentTransform(Id);
 
-    //    var cursor = new Vector2(e.touchData.WorldPoint.X, e.touchData.WorldPoint.Y);
+        var cursor = new Vector2(e.touchData.WorldPoint.X, e.touchData.WorldPoint.Y);
 
-    //    var featureCenter = Vector2.Transform(
-    //        new Vector2(Size.Width * 0.5f, Size.Height * 0.5f),
-    //        worldTransform);
+        var featureCenter = Vector2.Transform(
+            new Vector2(Size.Width * 0.5f, Size.Height * 0.5f),
+            worldTransform);
 
-    //    var deltaX = cursor.X - featureCenter.X;
-    //    var deltaY = cursor.Y - featureCenter.Y;
+        var deltaX = cursor.X - featureCenter.X;
+        var deltaY = cursor.Y - featureCenter.Y;
 
-    //    Console.WriteLine($"{deltaX},{deltaY}");
+        Console.WriteLine($"{deltaX},{deltaY}");
 
-    //    var angleRadians = MathF.Atan2(deltaY, deltaX);
-    //    var angleDegrees = angleRadians * (180f / MathF.PI) - 90f;
+        var angleRadians = MathF.Atan2(deltaY, deltaX);
+        var angleDegrees = angleRadians * (180f / MathF.PI) - 90f;
 
-    //    Transform.Rotation = angleDegrees;
+        Transform.Rotation = angleDegrees;
 
-    //    Scene.InvalidateTransform(Id);
+        Scene.InvalidateTransform(Id);
 
-    //    DebugCenter = new Vector2(
-    //        e.touchData.ParentPoint.X,
-    //        e.touchData.ParentPoint.Y + HANDLE_OFFSET
-    //    );
-    //}
+        DebugCenter = new Vector2(
+            e.touchData.ParentPoint.X,
+            e.touchData.ParentPoint.Y + HANDLE_OFFSET
+        );
+    }
     #endregion
 
     private class NodeRenderer : INodeRenderer
